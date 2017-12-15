b68ab99987d8982635c5c34900c8fd7b07cf6ab2
Christophe Ronco <c.ronco@kerlink.fr>
2017-12-15 10:34:22 +0100

    service: Implement option to enable auto connection for roaming services

--- src/service.c
+++ src/service.c
@@ -3809,7 +3809,8 @@ static bool is_ignore(struct connman_service *service)
 	if (!service->autoconnect)
 		return true;
 
-	if (service->roaming)
+	if (service->roaming &&
+		!connman_setting_get_bool("AutoConnectRoamingServices"))
 		return true;
 
 	if (service->ignore)
