Ultimaker/connman

commit 8810e72176f86bd5425c7f2e6355f1d0bd52c2a7

inet: Return error if pnp_file doesn't exists
If pnp_file doesn't exists then its contents were not read and pnp
variable remained NULL. Later when pnp variable was passed to
g_strsplit() function then below GLib warning occured:
(connmand:32767): GLib-CRITICAL **: g_strsplit: assertion 'string != NULL' failed
connmand[32767]: get_nfs_server_ip: Cannot split pnp at file "/proc/net/pnp"

This patch returns error if pnp_file doesn't exists

src/inet.c
@@ -2997,10 +2997,14 @@ static int get_nfs_server_ip(const char *cmdline_file, const char *pnp_file,
 		goto out;
 	}
 
-	if (g_file_test(pnp_file, G_FILE_TEST_EXISTS) &&
-			!g_file_get_contents(pnp_file, &pnp, NULL, &error)) {
-		connman_error("%s: Cannot read %s %s\n", __func__,
-				pnp_file, error->message);
+	if (g_file_test(pnp_file, G_FILE_TEST_EXISTS)) {
+		if (!g_file_get_contents(pnp_file, &pnp, NULL, &error)) {
+			connman_error("%s: Cannot read %s %s\n", __func__,
+						  pnp_file, error->message);
+			goto out;
+		}
+	} else {
+		connman_error("%s: File %s doesn't exist\n", __func__, pnp_file);
 		goto out;
 	}
 

commit 39d8e425a3468b9fbbbf3cacdde5f085de142ad7

inet: Remove error message if /proc/net/pnp file is missing
Commit 8810e72176f8 ("inet: Return error if pnp_file doesn't exists")
introduced the error message. For system without an NFS root this
is not an error. Avoid printing this message on every bootup.

Suggested-by: Vasyl Vavrychuk <vvavrychuk@gmail.com>

src/inet.c
@@ -3034,10 +3034,8 @@ static int get_nfs_server_ip(const char *cmdline_file, const char *pnp_file,
 						  pnp_file, error->message);
 			goto out;
 		}
-	} else {
-		connman_error("%s: File %s doesn't exist\n", __func__, pnp_file);
+	} else
 		goto out;
-	}
 
 	len = strlen(cmdline);
 	if (len <= 1) {