dc84b31503f8c65bce82ce9d9e5ac3e34332be23
Raymond Siudak <r.siudak@ultimaker.com>
2021-04-21 10:14:01 +0200

    Dockerize build and compile for arm64
    Signed-off-by: Raymond Siudak <r.siudak@ultimaker.com>

--- build_for_ultimaker.sh
+++ build_for_ultimaker.sh
@@ -1,46 +1,97 @@
-#!/bin/bash
+#!/bin/sh
+# Copyright (C) 2019 Ultimaker B.V.
+#
 
 set -eu
 
-ARCH="${ARCH:-armhf}"
+LOCAL_REGISTRY_IMAGE="connman"
+
+ARCH="${ARCH:-"arm64"}"
 SRC_DIR="$(pwd)"
-BUILD_DIR="${SRC_DIR}/_build"
-DEB_DIR="${BUILD_DIR}/_install"
-PACKAGE_NAME="${PACKAGE_NAME:-connman}"
-SYSCONFDIR="${SYSCONFDIR:-/etc}"
-MODULES_LOAD_DIR="${SYSCONFDIR}/modules-load.d"
+RELEASE_VERSION="${RELEASE_VERSION:-}"
+DOCKER_WORK_DIR="/build"
+BUILD_DIR_TEMPLATE="_build"
+BUILD_DIR="${BUILD_DIR_TEMPLATE}"
 
-if [ -z "${RELEASE_VERSION+x}" ]; then
-	RELEASE_VERSION="9999.99.99"
+update_docker_image()
+{
+    echo "Building local Docker build environment."
+    docker build ./docker_env -t "${LOCAL_REGISTRY_IMAGE}"
+}
+
+run_in_docker()
+{
+    docker run \
+        --privileged \
+        --rm \
+        -it \
+        -u "$(id -u)" \
+        -e "BUILD_DIR=${DOCKER_WORK_DIR}/${BUILD_DIR}" \
+        -e "ARCH=${ARCH}" \
+        -e "RELEASE_VERSION=${RELEASE_VERSION}" \
+        -e "MAKEFLAGS=-j$(($(getconf _NPROCESSORS_ONLN) - 1))" \
+        -v "${SRC_DIR}:${DOCKER_WORK_DIR}" \
+        -w "${DOCKER_WORK_DIR}" \
+        "${LOCAL_REGISTRY_IMAGE}" \
+        "${@}"
+}
+
+run_build()
+{
+    run_in_docker "./build.sh" "${@}"
+}
+
+deliver_pkg()
+{
+    cp "${BUILD_DIR}/"*".deb" "./"
+}
+
+run_tests()
+{
+    echo "There are no tests available for this repository."
+}
+
+usage()
+{
+    echo "Usage: ${0} [OPTIONS]"
+    echo "  -c   Clean the workspace"
+    echo "  -h   Print usage"
+    echo
+    echo "Other options will be passed on to build.sh"
+    echo "Run './build.sh -h' for more information."
+}
+
+while getopts ":cChlt" options; do
+    case "${options}" in
+    c)
+        run_build "${@}"
+        exit 0
+        ;;
+    h)
+        usage
+        exit 0
+        ;;
+    :)
+        echo "Option -${OPTARG} requires an argument."
+        exit 1
+        ;;
+    ?)
+        echo "Invalid option: -${OPTARG}"
+        exit 1
+        ;;
+    esac
+done
+shift "$((OPTIND - 1))"
+
+if ! command -V docker; then
+    echo "Docker not found, docker-less builds are not supported."
+    exit 1
 fi
 
-./bootstrap
+update_docker_image
 
-# Start with a clean build
-if [ -d "${BUILD_DIR}" ] && [ -z "${BUILD_DIR##*_build*}" ]; then
-    rm -rf "${BUILD_DIR}"
-fi
+run_build "${@}"
 
-mkdir "${BUILD_DIR}"
-cd "${BUILD_DIR}"
+deliver_pkg
 
-CFLAGS=--sysroot="${SYSROOT}" LDFLAGS=--sysroot="${SYSROOT}" PKG_CONFIG="${SYSROOT}/../arm-pkg-config ${SYSROOT}" \
-    ../configure --with-systemdunitdir=/lib/systemd/system --enable-polkit --prefix=/usr --localstatedir=/var \
-	--build="$(gcc -dumpmachine)" --host=arm-linux-gnueabihf --with-sysroot="${SYSROOT}" --with-libtool-sysroot="${SYSROOT}"
-
-make
-make install DESTDIR="${DEB_DIR}"
-
-mkdir "${DEB_DIR}/DEBIAN"
-
-sed -e 's|@ARCH@|'"${ARCH}"'|g' \
-        -e 's|@PACKAGE_NAME@|'"${PACKAGE_NAME}"'|g' \
-        -e 's|@RELEASE_VERSION@|'"${RELEASE_VERSION}"'|g' \
-        "${SRC_DIR}/debian/control.in" > "${DEB_DIR}/DEBIAN/control"
-
-cp "${SRC_DIR}/debian/postinst" "${DEB_DIR}/DEBIAN/postinst"
-
-mkdir -p "${DEB_DIR}/${MODULES_LOAD_DIR}"
-cp "${SRC_DIR}/config/modules-load.d/connman.conf" "${DEB_DIR}/${MODULES_LOAD_DIR}/"
-
-fakeroot dpkg-deb --build "${DEB_DIR}" ../connman-${RELEASE_VERSION}_armhf.deb
+exit 0
